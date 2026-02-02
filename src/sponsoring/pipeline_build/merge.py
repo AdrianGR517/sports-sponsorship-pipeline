d6ca50d4a1ae2190c7223f867a4af509256b510c
AdrianGR517 <adriangr591@gmail.com>
2026-02-02 16:11:57 +0100

    Now the build_merge funcion merges more than 2 dfs

--- src/sponsoring/pipeline_build/merge.py
+++ src/sponsoring/pipeline_build/merge.py
@@ -5,14 +5,24 @@ import logging
 logger = logging.getLogger(__name__)
 
 def build_merge(dfs, column_merge= 'deal_id'):
-    if any([column_merge not in df.columns for df in dfs]):
-        raise typer.BadParameter("column " + column_merge + " not found in both dataframes")
-    
-    logger.info("Merging the dataframes")
 
-    df1, df2 = dfs[0], dfs[1]
-    df_merged = pd.merge(df1,df2,how='inner',on=column_merge)
+    if not dfs:
+        raise typer.BadParameter("no dataframes found")
     
-    logger.info("Dataframes successfully merged")
+    elif len(dfs) == 1:
+        logger.info("Only one dataframe found, no need to merge")
+        
+        df_merged = dfs[0]
+    
+    else:
+        if any([column_merge not in df.columns for df in dfs]):
+            raise typer.BadParameter("column " + column_merge + " not found in every dataframe")
+    
+        logger.info(f"{len(dfs)} dataframes found. Merging the dataframes")
+
+        df1 = dfs[0] 
+        df_merged = pd.merge(df1, build_merge(dfs[1:],column_merge=column_merge),how='inner',on=column_merge)
+        
+        logger.info("Dataframes successfully merged")
 
     return(df_merged)
